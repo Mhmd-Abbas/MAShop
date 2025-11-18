e15a7051f3bf107b59adeca7410a6655eb1f8a5e
Mhmd-abbas <mohammadtheeb888@gmail.com>
2025-11-18 19:24:15 +0200

    first

---
+++ MAShop.DAL/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAShop.DAL.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
