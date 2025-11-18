55b423cba5948589293658247a0a85e324f63414
Mhmd-abbas <mohammadtheeb888@gmail.com>
2025-11-18 19:24:15 +0200

    first

---
+++ MAShop.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using MAShop.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAShop.DAL.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
---
+++ MAShop.PL/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using MAShop.DAL.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MAShop.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoriesController(ApplicationDbContext context) 
+        {
+            this.context = context;
+        }
+    }
+}
